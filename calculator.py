583ab1b8197e62d10799e4020c83de062c2f2fd1
loharvishal108-ui <loharvishal108@gmail.com>
2026-06-09 09:39:44 +0530

    Update and rename C:\Users\lohar\OneDrive\Desktop\PROJECTS\calculator.py to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,25 +1,3 @@
-# num1 = float(input("enter number 1 :"))
-# num2 = float(input("enter number 2 :"))
-
-# choice = input("Enter operation :")   # * , - , / , +
-
-# if choice == "+":                     # addition  
-#     print("Result :" , num1 + num2)
-# elif choice == "-":                   # subtraction  
-#     print("Result :" , num1 - num2)
-# elif choice == "*" :                  # multiplication  
-#     print("Result :" , num1 * num2)
-# elif choice == "/":                    # division
-#     if num2 !=0:
-#         print("Result :" , num1/num2)
-#     else:
-#         print("cannot divide by 0")
-
-
-
-
-
-
 #  CALCULATOR
 num1 = float(input("enter number 1 :"))
 num2 = float(input("enter number 2 :"))
